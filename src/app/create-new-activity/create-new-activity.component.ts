72458157043122f892232b3356931cc42d007a25
Florian Spier <mail@florian-spier.de>
2017-09-16 15:09:43 +0200

    Use scss, add bootstrap

--- src/app/create-new-activity/create-new-activity.component.ts
+++ src/app/create-new-activity/create-new-activity.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-new-activity',
   templateUrl: './create-new-activity.component.html',
-  styleUrls: ['./create-new-activity.component.css']
+  styleUrls: ['./create-new-activity.component.scss']
 })
 export class CreateNewActivityComponent implements OnInit {
 
