1e218d54f82d4e370b7ba2eb81668164b0c8f42c
Florian Spier <florian.spier@kbc.be>
2017-09-26 22:11:51 +0200

    Activity: calculate total, add member select

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../data.service';
 import { Activity } from '../model/activity';
 import {Member} from "../model/member";
+import {Expense} from "../model/expense";
 
 @Component({
   selector: 'app-activity',
@@ -13,6 +14,7 @@ export class ActivityComponent implements OnInit {
 
   activity:Activity = new Activity();
   currentMember:Member;
+  total:number = 0;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -25,22 +27,51 @@ export class ActivityComponent implements OnInit {
     if(this.activity == null) {
       this.dataService.getActivityById(id)
         .subscribe(activity => this.onActivityReceived(activity));
+    } else {
+      this.getCurrentMember();
+      this.total = this.calculateTotal();
     }
   }
 
-  private onActivityReceived(activity:Activity) {
+  private onActivityReceived(activity:Activity):void {
     this.activity = activity;
     this.dataService.setCurrentActivity(activity);
+    //TODO avoid duplicate code (see getActivity function)
+    this.getCurrentMember();
+    this.total = this.calculateTotal();
+  }
+
+  private getCurrentMember():void{
     this.currentMember = this.dataService.getCurrentMember();
+
+    if(this.currentMember == null){
+      this.currentMember = this.activity.members[0];
+    }
+
+    this.dataService.setCurrentMember(this.currentMember);
   }
 
-  selectMember(member) {
-    this.dataService.setCurrentMember(member);
-    this.currentMember = member;
+  private calculateTotal():number {
+    var total:number = 0;
+    this.activity.members.forEach(function(member:Member){
+      member.expenses.forEach(function(expense:Expense){
+        console.log(total, expense.amount)
+        total+=Number(expense.amount);
+      })
+    });
+    return total;
   }
 
-  goToExpensesPage() {
+  goToExpensesPage():void {
     this.router.navigateByUrl('/add-expense');
+  }
+
+  goToExpensesOverviewPage():void {
+    this.router.navigateByUrl('/overview-expenses');
+  }
+
+  onMemberSelected(member:Member):void{
+    this.dataService.setCurrentMember(member);
   }
 
   ngOnInit() {
